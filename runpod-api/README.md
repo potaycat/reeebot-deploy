936a366b82afab22a0531929cad21d38e8580b5f
long_nh <long_nh@flinters.vn>
2023-03-21 22:28:50 +0700

    Update
    Update Dockerfile
    
    Update Dockerfile
    
    Update start.sh
    
    Update start.sh
    
    upda
    
    Update start.sh
    
    Update Dockerfile
    
    Update Dockerfile
    
    Update Dockerfile
    
    Update Dockerfile
    
    Update Dockerfile
    
    Update Dockerfile

---
+++ runpod-api/README.md
@@ -0,0 +1,6 @@
+
+```
+git fetch && git pull && docker build .
+docker tag 41d3254ba31e longnhfvtap/lucario-sd:1.0
+docker push longnhfvtap/lucario-sd:1.0r2
+```
--- runpod-api/start.sh
+++ runpod-api/start.sh
@@ -3,8 +3,12 @@ echo "Container Started"
 export PYTHONUNBUFFERED=1
 source /workspace/stable-diffusion-webui/venv/bin/activate
 cd /workspace/stable-diffusion-webui
+
+wget -ON workspace/stable-diffusion-webui/webui.py \
+    https://gist.githubusercontent.com/potaycat/b3713e593dbe9b3570beea6239356f1d/raw/917b2c3fb6a8068ae39b6b688110391ef3eb767d/webui.py
 echo "starting api"
-python webui.py --port 3000 --nowebui --api --xformers --ckpt /workspace/stable-diffusion-webui/models/Stable-diffusion/anything-v4.5-pruned.safetensors &
+python webui.py --port 3000 --nowebui --api --xformers \
+    --ckpt models/Stable-diffusion/anything-v4.5-pruned.safetensors &
 cd /
 
 echo "starting worker"
