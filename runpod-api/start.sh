eb8880f6f6614e11be8e9d7418bbc41b0c461676
long_nh <long_nh@flinters.vn>
2023-03-21 21:06:04 +0700

    fly

--- runpod-api/start.sh
+++ runpod-api/start.sh
@@ -4,7 +4,7 @@ export PYTHONUNBUFFERED=1
 source /workspace/stable-diffusion-webui/venv/bin/activate
 cd /workspace/stable-diffusion-webui
 echo "starting api"
-python webui.py --port 3000 --nowebui --api --xformers --ckpt /model.safetensors &
+python webui.py --port 3000 --nowebui --api --xformers --ckpt /workspace/stable-diffusion-webui/models/Stable-diffusion/anything-v4.5-pruned.safetensors &
 cd /
 
 echo "starting worker"
