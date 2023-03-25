938e20da057b6d64844692903e8a08b4d9b66874
long_nh <long_nh@flinters.vn>
2023-03-26 06:44:40 +0700

    a

--- runpod-api/README.md
+++ runpod-api/README.md
@@ -1,5 +1,7 @@
 
 ```
+git clone https://github.com/potaycat/reeebot-deploy.git
+cd reeebot-deploy/runpod-api/
 git fetch && git pull && docker build .
 docker tag 41d3254ba31e longnhfvtap/lucario-sd:1.0
 docker push longnhfvtap/lucario-sd:1.0r2
--- runpod-api/start.sh
+++ runpod-api/start.sh
@@ -4,11 +4,9 @@ export PYTHONUNBUFFERED=1
 source /workspace/stable-diffusion-webui/venv/bin/activate
 cd /workspace/stable-diffusion-webui
 
-wget -ON workspace/stable-diffusion-webui/webui.py \
-    https://gist.githubusercontent.com/potaycat/b3713e593dbe9b3570beea6239356f1d/raw/917b2c3fb6a8068ae39b6b688110391ef3eb767d/webui.py
 echo "starting api"
 python webui.py --port 3000 --nowebui --api --xformers \
-    --ckpt models/Stable-diffusion/anything-v4.5-pruned.safetensors &
+    --ckpt models/Stable-diffusion/Kemono.safetensors &
 cd /
 
 echo "starting worker"
